80bf3d06816e9c003af5748494e7991379644c47
vovaZuev <vovazuev@yandex.ru>
2021-03-23 02:28:49 +0700

    First class DONE

---
+++ Intro/Intro/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x() const
+	{
+		return x;
+	}
+	double get_y() const
+	{
+		return y;
+	}
+	void set_x(double value)
+	{
+		this->x = value;
+	}
+	void set_y(double value)
+	{
+		this->y = value;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	Point A;
+	A.set_x(5);
+	A.set_y(17);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point* Pa = &A;
+	cout << Pa->get_x() << "\t" << Pa->get_y() << endl;
+}
