5a8f8e245233f86e986ae2d465800c97f2d05426
vovaZuev <vovazuev@yandex.ru>
2021-03-25 18:44:00 +0700

    Class work

--- Intro/Intro/main.cpp
+++ Intro/Intro/main.cpp
@@ -22,16 +22,43 @@ public:
 	{
 		this->y = value;
 	}
+
+	// Constructors
+
+	Point()
+	{
+		x = y = 0;
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	// Methods
+	void print() const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
 };
 
 int main()
 {
 	setlocale(LC_ALL, "");
 	Point A;
-	A.set_x(5);
+	/*A.set_x(5);
 	A.set_y(17);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
 
 	Point* Pa = &A;
-	cout << Pa->get_x() << "\t" << Pa->get_y() << endl;
+	cout << Pa->get_x() << "\t" << Pa->get_y() << endl;*/
+	A.print();
+	Point B(3, 2);
+	B.print();
 }
