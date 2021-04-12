7288c8300e46085c6a8b3247845d675d42594398
vovaZuev <vovazuev@yandex.ru>
2021-04-13 01:23:18 +0700

    String started

--- Intro/String/main.cpp
+++ Intro/String/main.cpp
@@ -1,6 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
+
+class String
+{
+	char* str; // Указатель на строку в динамической памяти
+	int size; // Размер строки в байтах
+public:
+	const char* get_str() const
+	{
+		return str;
+	}
+	int get_size() const
+	{
+		return size;
+	}
+	// Constructors
+	String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "Default constructor:\t" << this << endl;
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		strcpy(this->str, str);
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		strcpy(this->str, other.str);
+		cout << "Copy Constructor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	// Operators
+	String& operator=(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		strcpy(this->str, other.str);
+		cout << "Copy Assignment:\t" << this << endl;
+		return *this;
+	}
+	// Methods
+	void print() const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+};
+
+ostream& operator << (ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
 int main()
 {
-
+	setlocale(LC_ALL, "");
+	String str;
+	str.print();
+	String str2 = "Hello";
+	str2.print();
+	cout << str2 << endl;
+	String str3 = str2;
+	cout << "str3:\t" << str3 << endl;
+	String str4;
+	str4 = str3;
+	cout << "str4:\t" << str4 << endl;
 }
