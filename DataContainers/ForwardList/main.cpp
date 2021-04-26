4ac4ea41b72c1f96cb53ed4497555818029ce000
vovaZuev <vovazuev@yandex.ru>
2021-04-27 02:07:16 +0700

    List DataContainer

---
+++ DataContainers/ForwardList/main.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Element
+{
+	int Data; // значение эл-та
+	Element* pNext; // указатель на следующий элемент
+public:
+	Element(int Data, Element* pNext = nullptr) : Data(Data), pNext(pNext)
+	{
+		cout << "ElementConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "ElementDestructor:\t" << this << endl;
+	}
+	friend class List;
+};
+
+class List
+{
+	Element* Head; // указатель на начальный элемент списка
+public:
+	List()
+	{
+		this->Head = nullptr;
+		cout << "ListConstructor:\t" << this << endl;
+	}
+	~List()
+	{
+		cout << "ListDestructor:\t" << this << endl;
+	}
+	// Adding elements
+	void push_front(int Data)
+	{
+		Element* New = new Element(Data);
+		New->pNext = Head;
+		Head = New;
+	}
+	// Methods
+	void print() const
+	{
+		// Для того, чтобы ходить по списку, нужны: 1) итератор, 2) Цикл
+		Element* temp = Head; // temp - это итератор - указатель, при помощи которого можно получить доступ к элементам структуры данных
+		while (temp != nullptr)
+		{
+			cout << temp << "\t" << temp->Data << "\t" << temp->pNext << endl;
+			temp = temp->pNext; // переход на следующий элемент
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+}
