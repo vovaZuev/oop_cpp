60ef3f71c875ed4f104c7ec6db6ecbb01fb7c03b
vovaZuev <vovazuev@yandex.ru>
2021-05-22 01:59:27 +0700

    Iterator sep

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -51,51 +51,63 @@ template <typename T> class Iterator
 {
 	Element<T>* Temp;
 public:
-	Iterator(Element<T>* Temp = nullptr) : Temp(Temp)
-	{
-		cout << "IConstructor:\t" << this << endl;
-	}
-	~Iterator()
-	{
-		cout << "IDestructor:\t" << this << endl;
-	}
+	Iterator(Element<T>* Temp = nullptr);
+	~Iterator();
 	// Operators
-	Iterator& operator++()
-	{
-		Temp = Temp->pNext;
-		return *this;
-	}
-	Iterator operator++(int)
-	{
-		Iterator old = *this;
-		Temp = Temp->pNext;
-		return old;
-	}
-	bool operator == (const Iterator& other) const
-	{
-		return this->Temp == other.Temp;
-	}
-	bool operator != (const Iterator& other) const
-	{
-		return this->Temp != other.Temp;
-	}
-	const Element<T>* operator -> () const
-	{
-		return Temp;
-	}
-	Element<T>* operator -> ()
-	{
-		return Temp;
-	}
-	const T& operator*() const
-	{
-		return Temp->Data;
-	}
-	T& operator*()
-	{
-		return Temp->Data;
-	}
+	Iterator& operator++();
+	Iterator operator++(int);
+	bool operator == (const Iterator& other) const;
+	bool operator != (const Iterator& other) const;
+	const Element<T>* operator -> () const;
+	Element<T>* operator -> ();
+	const T& operator*() const;
+	T& operator*();
 };
+
+template <typename T> Iterator<T>::Iterator(Element<T>* Temp) : Temp(Temp)
+{
+	cout << "IConstructor:\t" << this << endl;
+}
+template <typename T> Iterator<T>::~Iterator()
+{
+	cout << "IDestructor:\t" << this << endl;
+}
+// Operators
+template <typename T> Iterator<T>& Iterator<T>::operator++()
+{
+	Temp = Temp->pNext;
+	return *this;
+}
+template <typename T> Iterator<T> Iterator<T>::operator++(int)
+{
+	Iterator old = *this;
+	Temp = Temp->pNext;
+	return old;
+}
+template <typename T> bool Iterator<T>::operator == (const Iterator& other) const
+{
+	return this->Temp == other.Temp;
+}
+template <typename T> bool Iterator<T>::operator != (const Iterator& other) const
+{
+	return this->Temp != other.Temp;
+}
+template <typename T> const Element<T>* Iterator<T>::operator -> () const
+{
+	return Temp;
+}
+template <typename T> Element<T>* Iterator<T>::operator -> ()
+{
+	return Temp;
+}
+template <typename T> const T& Iterator<T>::operator*() const
+{
+	return Temp->Data;
+}
+template <typename T> T& Iterator<T>::operator*()
+{
+	return Temp->Data;
+}
 
 template <typename T> class ForwardList
 {
